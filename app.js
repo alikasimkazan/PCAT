04018e90f0853696c1e83c6c68a051c9869a26ca
Ali Kasım <alikasimkazan0@gmail.com>
2024-09-03 03:31:10 +0300

    MVC pattern implemented

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 const Photo = require('./models/Photo')
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController')
 
 const app = express();
 
@@ -13,33 +18,28 @@ mongoose.connect('mongodb://localhost/pcat-test-db');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 
 
 // TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
 
 
-// ROUTE
+// ROUTES
 
-app.get('/', async (req, res) => {
-    const photos= await Photo.find({})
-    res.render('index',{
-        photos,
-    });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
 
-app.get('/add', (req, res) => {
-    res.render('add');
-});
-
-app.post('/photos', async(req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')
-})
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 
 const port = 3000;
